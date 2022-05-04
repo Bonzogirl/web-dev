8275b5e6cb8a62957bc83e1444ca8c22def45e36
Bonzogirl01~ <10fylhe@gmail.com>
2022-05-04 16:16:32 +0700

    mozolit glasa

--- ejs challenge/app.js
+++ ejs challenge/app.js
@@ -10,6 +10,7 @@ const contactContent =
   'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
 const app = express();
+const posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -30,6 +31,18 @@ app.get('/contact', (req, res) => {
   res.render('contact', {
     contactContent: contactContent,
   });
+});
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/compose', function (req, res) {
+  var post = {
+    title: req.body.title,
+    content: req.body.postBody,
+  };
+  posts.push(post);
+  res.redirect('/');
 });
 
 app.listen(5000, function () {
